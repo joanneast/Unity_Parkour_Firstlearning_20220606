c99d94bd9d8d0b9676464284da3524d4bc1e1290
joanneast <105775355+joanneast@users.noreply.github.com>
2022-06-20 10:11:40 +0800

    跑酷 練習API

--- Unity_Parkour_Firstlearning20220606/Assets/C#Scripts/APIStatic.cs
+++ Unity_Parkour_Firstlearning20220606/Assets/C#Scripts/APIStatic.cs
@@ -7,6 +7,9 @@ namespace Parkour
     /// </summary>
     public class APIStatic : MonoBehaviour
     {
+        private Vector3 a = new Vector3(1, 1, 1);
+        private Vector3 b = new Vector3(22, 22, 22);
+
         private void Start()
         {
             #region 學習
@@ -50,11 +53,28 @@ namespace Parkour
             #region 練習
 
             // 計算所有攝影機數量
-            int camcount = Camera.allCamerasCount;
-            print("目前攝影機：" + camcount + "台");
+            print("目前攝影機：" + Camera.allCamerasCount + "台");
 
             //顯示遊戲平台
-            
+            print("目前遊戲平台：" + Application.platform);
+
+            //3D物理睡眠臨界值設定為10
+            Physics.sleepThreshold = 10;
+            print("物理睡眠臨界值：" + Physics.sleepThreshold);
+
+            //時間大小設定為0.5(慢動作)
+            Time.timeScale = 0.5f;
+            print("時間速度：" + Time.timeScale);
+
+            //對9.999去小數點
+            print("對9.999四捨五入：" + Mathf.Round(9.999f));
+
+            //取得兩點的距離
+            float distance = Vector3.Distance(a,b);
+            print("ab點之間距離：" + distance);
+
+            //開啟連結
+            Application.OpenURL("https://unity.com/");
 
             #endregion
         }
@@ -72,6 +92,14 @@ namespace Parkour
             #endregion
 
             #region 練習
+            //是否輸入任意鍵
+            //print("是否輸入任意鍵：" + Input.anyKeyDown);
+
+            //遊戲經過時間
+            //print("遊戲經過時間：" + Time.timeSinceLevelLoad);
+
+            //是否按下空白鍵
+            print("是否按下空白鍵：" + Input.GetKeyDown(KeyCode.Space));
 
             #endregion
         }
